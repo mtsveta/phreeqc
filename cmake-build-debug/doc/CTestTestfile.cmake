eeb6410e239d42ea57f3df89e9ea55812f14c9ac
Svetlana Kyas <mtsveta@gmail.com>
2020-09-18 16:24:16 +0200

    Add Shells databses

--- cmake-build-debug/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kyas/phreeqc-3.6.2-15100/doc
-# Build directory: /Users/kyas/phreeqc-3.6.2-15100/cmake-build-debug/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
